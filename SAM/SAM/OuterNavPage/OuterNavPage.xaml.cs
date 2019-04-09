2b90d76fdec1543aace01b9a6e3522e6b5d3ad6f
Matthew Owen <mattowen@microsoft.com>
2019-04-09 16:08:30 -0700

    Add outernav

---
+++ SAM/SAM/OuterNavPage/OuterNavPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace SAM.OuterNav
+{
+    public sealed partial class OuterNavPage : Page
+    {
+        public OuterNavPage(OuterNavViewModel viewModel)
+        {
+            if (viewModel == null)
+            {
+                throw new ArgumentNullException("viewModel");
+            }
+            _viewModel = viewModel;
+
+            this.InitializeComponent();
+        }
+
+        private OuterNavViewModel _viewModel;
+        public OuterNavViewModel ViewModel
+        {
+            get { return _viewModel; }
+        }
+    }
+}
---
+++ SAM/SAM/OuterNavPage/OuterNavViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SAM.OuterNav
+{
+    public class OuterNavViewModel : INotifyPropertyChanged
+    {
+        public OuterNavViewModel()
+        {
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void RaisePropertyChangedFromSource([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
