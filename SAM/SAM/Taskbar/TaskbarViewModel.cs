9325cc778fac4d2a1745a1a698974ba20f426888
Matthew Owen <mattowen@microsoft.com>
2019-04-09 17:00:43 -0700

    Example of using xbind for button click

--- SAM/SAM/Taskbar/TaskbarViewModel.cs
+++ SAM/SAM/Taskbar/TaskbarViewModel.cs
@@ -5,11 +5,17 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
 namespace SAM.Taskbar
 {
     public class TaskbarViewModel : INotifyPropertyChanged
     {
+        public void OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         #region INotifyPropertyChanged
 
         public event PropertyChangedEventHandler PropertyChanged;
